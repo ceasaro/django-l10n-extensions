033dba731a52dff4200a8974c8dc59f6c21bd919
ceasaro <ceesvw@gmail.com>
2023-05-16 15:23:28 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-l10n-extensions',
-    version='1.0.7',
+    version='1.0.8-alpha',
     author=u'Cees van Wieringen',
     author_email='ceesvw@gmail.com',
     package_dir={'': 'src'},

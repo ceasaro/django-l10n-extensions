27c9883cfe956157b77b72026bcf13f6864f502d
ceasaro <ceesvw@gmail.com>
2024-04-15 14:01:48 +0200

    fix imports

--- src/tests/testapp/urls.py
+++ src/tests/testapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth import login, logout
 from django.views.generic import TemplateView
 
 from tests.testapp.views import TransExampleView
